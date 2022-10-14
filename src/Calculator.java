1821d7624d582481da9270112563260d7a2d934a
victor <victor.m.weiss@gmail.com>
2022-10-13 18:08:42 -0700

    new branch

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,147 +1,70 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Calculator {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         //ask user for the calculation
-
         Scanner calculation = new Scanner(System.in);
         System.out.println("pleas enter your four function calculation");
         String userCalculation = calculation.nextLine();
-
         System.out.println(userCalculation);
 
         //convert to int and remove spaces
-
         String something = userCalculation;
         int length = something.length();
-        String numOne = "";
-        String numTwo = "";
-        String operation = "";
-        boolean firstNum = true;
+        ArrayList<String> mathProblem = new ArrayList<String>();
+        String num = "";
 
-         for (int i = 0; i < length; i++) {
+        for (int i = 0; i < length; i++) {
             char character = something.charAt(i);
-            if (Character.isDigit(character)&& firstNum) {
-                numOne += character;
+            if (Character.isDigit(character)) {
+                num += character;
+                if (i == length - 1) {
+                    mathProblem.add(num);
+                }
+            } else {
+                mathProblem.add(num);
+                num = "";
+                String operation = "" + character;
+                mathProblem.add(operation);
             }
-            else if (!Character.isDigit(character)){
-                firstNum = false;
-                operation += character;
+        }
+
+        System.out.println(mathProblem);
+
+        int size = mathProblem.size();
+        for (int i = 0; i < size; i++) {
+            char character = something.charAt(i);
+            if (!Character.isDigit(character)) {
+                String operation = "" + character;
+            } else {
+                System.out.println("no oporator");
             }
-            else{
-                numTwo += character;
-            }
         }
-        System.out.println(numOne);
-        System.out.println(numTwo);
-        System.out.println(operation);
-
-        //num in int
-        double numOneInt = Integer.parseInt(numOne);
-        double numOneTwo = Integer.parseInt(numTwo);
-
-
-
-        //calculate
-
-        if (operation.equals("+")){
-            double ansOne = (numOneInt+numOneTwo);
-            System.out.println(ansOne);
-        }
-        else if (operation.equals("-")){
-            double ansOne = (numOneInt-numOneTwo);
-            System.out.println(ansOne);
-        }
-        else if (operation.equals("*")){
-            double ansOne = (numOneInt*numOneTwo);
-            System.out.println(ansOne);
-        }
-        else if (operation.equals("/")){
-            double ansOne = (numOneInt/numOneTwo);
-            System.out.println(ansOne);
-        }
-        else{
-            System.out.println("oops somthing went wrong");
-        }
-
-
-
-        //print
-
     }
 }
+//        //num in int
+//        double numOneInt = Integer.parseInt(numOne);
+//        double numOneTwo = Integer.parseInt(numTwo);
 
-//import java.io.*;
-//import java.lang.*;
-//import java.lang.Math;
-//import java.util.Scanner;
-//public class BasicCalculator {
-//
-//    public static void main(String[] args)
-//    {
-//        // stores two numbers
-//        double num1, num2;
-//
-//        // Take input from the user
-//        Scanner sc = new Scanner(System.in);
-//
-//        System.out.println("Enter the numbers");
-//
-//        // take the inputs
-//        num1 = sc.nextDouble();
-//
-//        num2 = sc.nextDouble();
-//
-//        System.out.println("Enter the operator (+,-,*,/)");
-//
-//        char op = sc.next().charAt(0);
-//
-//        double o = 0;
-//
-//        switch (op) {
-//
-//            // case to add two numbers
-//            case '+':
-//
-//                o = num1 + num2;
-//
-//                break;
-//
-//            // case to subtract two numbers
-//            case '-':
-//
-//                o = num1 - num2;
-//
-//                break;
-//
-//            // case to multiply two numbers
-//            case '*':
-//
-//                o = num1 * num2;
-//
-//                break;
-//
-//            // case to divide two numbers
-//            case '/':
-//
-//                o = num1 / num2;
-//
-//                break;
-//
-//            default:
-//
-//                System.out.println("You enter wrong input");
-//
-//                break;
+//        //calculate
+//        if (operation.equals("+")){
+//            double ansOne = (numOneInt+numOneTwo);
+//            System.out.println(ansOne);
 //        }
-//
-//        System.out.println("The final result:");
-//
-//        System.out.println();
-//
-//        // print the final result
-//        System.out.println(num1 + " " + op + " " + num2
-//                + " = " + o);
-//    }
-//}
+//        else if (operation.equals("-")){
+//            double ansOne = (numOneInt-numOneTwo);
+//            System.out.println(ansOne);
+//        }
+//        else if (operation.equals("*")){
+//            double ansOne = (numOneInt*numOneTwo);
+//            System.out.println(ansOne);
+//        }
+//        else if (operation.equals("/")){
+//            double ansOne = (numOneInt/numOneTwo);
+//            System.out.println(ansOne);
+//        }
+//        else{
+//            System.out.println("oops somthing went wrong");
+//        }
